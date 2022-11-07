86b8b3b557416f65ba52d51304934a12bcd51bad
blackLead8 <achristopherdev@gmail.com>
2022-11-07 11:33:47 -0300

    Criando o servidor

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Executei");
+// importando o express e os tipos Request, Response e NextFunction
+
+import express, { Request, Response, NextFunction } from 'express';
+const app = express();
+
+//criando uma rota
+app.get("/status" , (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).send({foo: 'bar'});
+
+
+});
+app.listen(3000, () =>{
+    console.log("Executando aplicação na porta 3000!");
+
+});
