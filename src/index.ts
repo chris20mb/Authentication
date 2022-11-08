f0ce908c9ba7943e8f1140a2db586a74ff5d231b
blackLead8 <achristopherdev@gmail.com>
2022-11-08 10:21:58 -0300

    Incluindo Rotas Put e Delete no código

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
 // importando o express e os tipos Request, Response e NextFunction
 
 import express, { Request, Response, NextFunction } from 'express';
+import statusRoute from './routes/status.route';
+import usersRoute from './routes/users.route';
 const app = express();
+//Configurações da aplicação
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
-//criando uma rota
-app.get("/status" , (req: Request, res: Response, next: NextFunction) => {
-    res.status(200).send({name: 'chris'});
+//Configurações de rotas
+app.use(usersRoute);
+app.use(statusRoute);
 
-
-});
+//Inicialização do servidor
 app.listen(3000, () =>{
     console.log("Executando aplicação na porta 3000!");
 
